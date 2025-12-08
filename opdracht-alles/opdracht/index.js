aa26079c8df4db6ad81cf9db41e6c11f025dea67
toasterbath0758 <toasterbath0758@gmail.com>
2025-12-08 09:34:25 +0100

    goede JS toegevoegd

--- opdracht-alles/opdracht/index.js
+++ opdracht-alles/opdracht/index.js
@@ -1,21 +1,41 @@
 
-const mobileNav=document.getElementById("mobileNav")
-function openMobileNav(){
-    mobileNav.classList.add("active")
-}
-function closeMobileNav(){
-    mobileNav.classList.remove("active")
-}
 
-var lastpos = 0;
-
-
-// const menuDoelgroep=document.getElementById("menuDoelgroep")
+// const mobileNav=document.getElementById("mobileNav")
 // function openMobileNav(){
-//     menuDoelgroep.classList.add("active")
+//     mobileNav.classList.add("active")
 // }
 // function closeMobileNav(){
-//     menuDoelgroep.classList.remove("active")
+//     mobileNav.classList.remove("active")
 // }
 
 // var lastpos = 0;
+
+
+//maak een knop die een hamburgermenu opent
+
+// const hamburgerMenuKnop=document.querySelector('click')
+// function openMobileNav(){
+    // mobileNav.classList.add("active")
+    // hamburgerMenuKnop.addEventListener('click', function()mobileNav)
+
+    // Stap 1
+let hamburgerMenuKnop = document.querySelector('.hamburgerMenuKnop');
+let mobileNav = document.querySelector('.mobileNav');
+
+// Stap 2
+hamburgerMenuKnop.addEventListener('click', function(){
+    console.log('textje')
+    mobileNav.classList.toggle('active')
+})
+
+
+function doeIets(){
+    console.log('deze moet t niet doen')
+    hamburgerMenuKnop.classList.toggle('active')
+}
+    // Stap 3
+    // document.querySelector('#hamburgerMenuKnop').classList.add('.mobileNav');
+// divaniNL
+
+//maak een knop die het hamburgermenu sluit
+
