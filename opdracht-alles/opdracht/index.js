6fba219b98f38ab062a3dbf5a17378ae6e796782
toasterbath0758 <toasterbath0758@gmail.com>
2025-12-12 12:37:34 +0100

    #5 fix mobile nav en sidemenu doelgroepfilter

--- opdracht-alles/opdracht/index.js
+++ opdracht-alles/opdracht/index.js
@@ -15,27 +15,41 @@
 
 // const hamburgerMenuKnop=document.querySelector('click')
 // function openMobileNav(){
-    // mobileNav.classList.add("active")
-    // hamburgerMenuKnop.addEventListener('click', function()mobileNav)
+// mobileNav.classList.add("active")
+// hamburgerMenuKnop.addEventListener('click', function()mobileNav)
 
-    // Stap 1
+
+// 
+// Stap 1 querySelector
 let hamburgerMenuKnop = document.querySelector('.hamburgerMenuKnop');
 let mobileNav = document.querySelector('.mobileNav');
 
-// Stap 2
+
+//maak een knop die het hamburgermenu opent en sluit
+// Stap 2 addEventListener
 hamburgerMenuKnop.addEventListener('click', function(){
-    console.log('textje')
+    mobileNav.classList.toggle('active')
+})
+
+let closeHamburgerMenuKnop = document.querySelector('.closeHamburgerMenuKnop');
+
+closeHamburgerMenuKnop.addEventListener('click', function(){
     mobileNav.classList.toggle('active')
 })
 
 
-function doeIets(){
-    console.log('deze moet t niet doen')
-    hamburgerMenuKnop.classList.toggle('active')
-}
-    // Stap 3
-    // document.querySelector('#hamburgerMenuKnop').classList.add('.mobileNav');
-// divaniNL
+//NIEUWE FUNCTIE VOOR DE DOD :)
+//maak de knop aan
+let openHetDoelgroepMenuKnop = document.querySelector('.openHetDoelgroepMenuKnop');
+let openDoelgroepMenu = document.querySelector('.openDoelgroepMenu');
+let knopDoelgroep = document.querySelector('.knopDoelgroep');
 
-//maak een knop die het hamburgermenu sluit
+//zet de clickfunction op de knop en de togglefunction
+openHetDoelgroepMenuKnop.addEventListener('click', function(){
+openDoelgroepMenu.classList.toggle('active');
+
+})
+// divaniNL //
+
+
 
